54f34e762ed4227e27093d89da8431aaf18dc89b
Napat Jirarattanakunchai <69129078+Napatji@users.noreply.github.com>
2020-08-23 13:57:35 +0700

    Add files via upload

---
+++ Lab2-2.c
@@ -0,0 +1,11 @@
+#define CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int num1, num2;
+	printf("Enter first number : ");
+	scanf("%d", &num1);
+	printf("Enter second number : ");
+	scanf("%d", &num2);
+	for()
+}
